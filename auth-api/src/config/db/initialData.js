310f53fde502ad5877beffe8317caae0eccca140
Yehia Azanki <yazanki@gmail.com>
2023-03-08 21:22:20 -0300

    Add User model and dbConfig

---
+++ auth-api/src/config/db/initialData.js
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+import User from "../../modules/user/model/User.js";
+
+export async function createInitialData() {
+    try {
+        await User.sync({ force: true });
+
+        let password = await bcrypt.hash("123456", 10);
+    
+        await User.create({
+            name: "Yehia Azanki",
+            email: "yazanki@gmail.com",
+            password: password,
+        });
+    } catch(err) {
+        console.error(err.message);
+    }
+    
+}
